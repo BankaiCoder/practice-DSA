085ecedfc5ae96dc08df16da6ce8451a2622e1e2
BankaiCoder <aman8882112318@gmail.com>
2024-02-06 15:04:27 +0530

    Programm to print array column wise in 2d array

---
+++ printMatrixColWise.java
@@ -0,0 +1,20 @@
+
+public class printMatrixColWise {
+    public static void main(String[] args) {
+        int[][] arr = { { 10, 20, 30, 40, 50 },
+                { 60, 70, 80, 90, 100 },
+                { 110, 120, 130, 140, 150 },
+                { 160, 170, 180, 190, 200 } };
+
+        int rows = arr.length;
+        int col = arr[0].length;
+
+        for (int j = 0; j < col; j++) {
+            for (int i = 0; i < rows; i++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
