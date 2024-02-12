7b358808f443433edb165d9c65bcf8a130444f8a
BankaiCoder <aman8882112318@gmail.com>
2024-02-12 12:54:06 +0530

    Bubble Sort

---
+++ bubbleSort.java
@@ -0,0 +1,24 @@
+public class bubbleSort {
+    public static void main(String[] args) {
+        int arr[] = { 1, 5, 3, 4, 9, 7 };
+        bubble(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void bubble(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
